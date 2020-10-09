a17ff08a6e9df1fe1e1667efc71c2d96e8bbfcf0
sankalp0210 <asankalp9@gmail.com>
2020-10-10 02:02:41 +0530

    dices generated and hash sent to contract

--- client/src/GameInformation.js
+++ client/src/GameInformation.js
@@ -1,11 +1,15 @@
 import React from "react";
+const { soliditySha3 } = require("web3-utils");
 
 class GameInformation extends React.Component {
     constructor() {
         super();
         this.state = {
             id : "",
-            registeredPlayers : JSON.parse(localStorage.getItem('registeredPlayers')) || {},
+            registeredPlayers : {},
+            playersDice : {},
+            playersDiceString : {},
+            playersNonce : {},
             numPlayersKey : null,
             playersKey : null,
             turnKey : null,
@@ -18,26 +22,58 @@ class GameInformation extends React.Component {
     }
 
     getInitialState = () => {
-        // console.log(localStorage.getItem('registeredPlayers'));
-        var registeredPlayers = JSON.parse(localStorage.getItem('registeredPlayers')) || {};
-        this.setState({registeredPlayers});
+        if(!localStorage.getItem('set')) return;
+
+        // retreiving data from local storage
+        var playersDice = JSON.parse(localStorage.getItem('playersDice'));
+        var playersDiceString = JSON.parse(localStorage.getItem('playersDiceString'));
+        var playersNonce = JSON.parse(localStorage.getItem('playersNonce'));
+        var registeredPlayers = JSON.parse(localStorage.getItem('registeredPlayers'));
+
+        this.setState({registeredPlayers, playersDice, playersDiceString, playersNonce});
     }
 
     register = (event)=> {
         const { drizzle, drizzleState } = this.props;
         const contract = drizzle.contracts.LiarsDice;
+        const curAccount = this.props.drizzleState.accounts[0];
 
         // let drizzle know we want to call the `registerParticipant` method from `account`
         const stackId = contract.methods["registerParticipant"].cacheSend({
-            from: drizzleState.accounts[0]
+            from: curAccount
         });
-        console.log(this.state.registeredPlayers);
-        let newregisteredPlayers = this.state.registeredPlayers;
-        console.log(newregisteredPlayers);
-        newregisteredPlayers[drizzleState.accounts[0]] = true;
-        localStorage.setItem('registeredPlayers', JSON.stringify(newregisteredPlayers));
-        // console.log(localStorage.getItem('registeredPlayers'));
-        this.setState({registeredPlayers : newregisteredPlayers});
+        
+        // registering the player
+        let registeredPlayers = this.state.registeredPlayers;
+        registeredPlayers[curAccount] = true;
+        let playersNonce = this.state.playersNonce;
+        playersNonce[curAccount] = 197; // needs to be changed to random
+        
+        // initialising the player's dice
+        const diceForCurPlayer = [1,6,2,5,3]; // need to change this to random
+        let playersDice = this.state.playersDice;
+        let playersDiceString = this.state.playersDiceString;
+        var data = "";
+        var i;
+        for(i=0;i<diceForCurPlayer.length;i++) data += diceForCurPlayer[i];
+        data += playersNonce[curAccount];
+        const hashDice = soliditySha3(data);
+        // sending the hashed value of dices to contract
+        contract.methods["setDice"].cacheSend( hashDice, {
+            from: curAccount
+        });
+        playersDice[curAccount] = diceForCurPlayer;
+        playersDiceString[curAccount] = data;
+
+        // setting items in local storage
+        localStorage.setItem('playersDice', JSON.stringify(playersDice));
+        localStorage.setItem('playersDiceString', JSON.stringify(playersDiceString));
+        localStorage.setItem('playersNonce', JSON.stringify(playersNonce));
+        localStorage.setItem('registeredPlayers', JSON.stringify(registeredPlayers));
+        localStorage.setItem('set', true);
+
+        // setting the state for the current changes
+        this.setState({registeredPlayers, playersDice, playersDiceString, playersNonce});
     }
 
     componentDidMount() {
@@ -75,9 +111,19 @@ class GameInformation extends React.Component {
         // );
 
         // if it exists, then we display its value
+        var myDice = []
         const curRegistered = this.state.registeredPlayers[curAccount];
+        if(curRegistered) myDice = this.state.playersDice[curAccount];
         if(curRegistered){
-            return <div><p>You have been successfully registered.</p></div>
+            return (
+                <div>
+                    <p>You have been successfully registered.</p>
+                    <p>My Connected Account : {curAccount}</p>
+                    <p>My dices are {myDice}</p>
+            </div>
+            );
+            return <div><p>You have been successfully registered.</p><br></br><br></br>
+            <p>Your dices are {myDice}</p></div>
         }
         return (
             <div>
